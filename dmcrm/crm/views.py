9f4d7d51a1134943a000b525588fce0366410262
kschary4103 <kschary4103@gmail.com>
2024-03-13 13:19:09 +0530

    updated views.py

--- dmcrm/crm/views.py
+++ dmcrm/crm/views.py
@@ -5,21 +5,21 @@ from django.http import HttpResponse, JsonResponse
 
 def home(request):
     try:
-        # Specify the path to the CSV file
+    # Specify the path to the CSV file
         csv_file_path = r'C:/Users/Akash Reddy/OneDrive/Documents/GitHub/crm/datasets/customer_information.csv'
-        
-        # Read the CSV file
+
+    # Read the CSV file
         with open(csv_file_path, 'r', encoding='utf-8') as file:
             csv_reader = csv.DictReader(file)
-            data = list(csv_reader)
-        
-        # Convert CSV data to JSON
-        json_data = json.dumps(data)
-        
-        return JsonResponse({'status': 'success', 'data': json_data})
-    
+            data = [row for row in csv_reader]
+
+    # Convert CSV data to JSON
+        json_data = json.dumps({'status': 'success', 'data': data})
+
+        return JsonResponse(json.loads(json_data))
+
     except FileNotFoundError:
         return JsonResponse({'status': 'error', 'message': 'File not found'}, status=404)
-    
+
     except Exception as e:
         return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
