aa32e4d5579ccbf8baf307e1a07a2a95535b6bd5
kschary4103 <104379514+klncpks@users.noreply.github.com>
2024-04-04 01:09:49 +0530

    done

--- dmcrm/crm/urls.py
+++ dmcrm/crm/urls.py
@@ -13,12 +13,14 @@ urlpatterns = [
     path('products/leastRevenue/',views.leastRevenue),
     path('products/<str:filter_type>/', views.extractList),
     path('customers/',views.customers),
-    path('customers/top5/',views.top5Cust),
+    path('customers/top5/',views.top5Customers),
     path('customers/count',views.customer_count_yearly),
     path('customers/<str:filter_type>/', views.extractList_customers),
-    path('product/<str:product_id>/', views.productDetails),
+    path('product/<str:product_id>/', views.product_details),
     path('product/purchases/<str:product_id>/',views.product_purchases),
     path('product/feedback/<str:product_id>/',views.product_feedback),
+    path('product/id/name/',views.extractProductIDsAndNames),
+    
 
 ]
 
